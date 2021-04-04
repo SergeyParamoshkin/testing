9db210d7c4397eb9a7c6edcc8b7263a842d7bd38
Sergey <serg3091@gmail.com>
2021-04-04 12:48:01 +0300

    add Dockerfile

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func GetRandomNumber(n int64) int64 {
 	return rand.Int63n(n)
 }
 
 func main() {
-
+	fmt.Println("run testing")
 }
